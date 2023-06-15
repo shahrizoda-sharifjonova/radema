9787a256294fe27c894e0ba857a7a7f947b097e7
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2023-06-15 15:30:48 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,20 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+const popupOpen = document.querySelector('.popup-open');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelectorAll('.popup-close');
+const body = document.querySelector('body');
+
+popupOpen.addEventListener('click', ()=>{
+    popup.classList.toggle('active')
+    body.classList.toggle('hidden')
+})
+
+popupClose.forEach(el => {
+    el.addEventListener('click', (e)=>{
+        popup.classList.remove('active')
+        body.classList.remove('hidden')
+    })
+});
